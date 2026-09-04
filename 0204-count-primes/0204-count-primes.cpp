c1e7080c11f0c0294125aa20c2a5860ceb4b85ee
Anurag kumar <114020159+anuragkrgupta@users.noreply.github.com>
2026-09-04 22:10:02 +0530

    Time: 559 ms (17.19%), Space: 248.9 MB (5.01%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -3,23 +3,14 @@ public:
     int countPrimes(int n) {
         if (n <= 2)
             return 0;
-
-        // Only store information for odd numbers
         vector<char> isComposite(n, false);
 
-        int count = 1; // 2 is prime
-
-        // Check only odd numbers
+        int count = 1;
         for (int i = 3; i < n; i += 2) {
 
             if (!isComposite[i]) {
                 count++;
-
-                // Start from i*i
-                // Only needed while i*i < n
                 if ((long long)i * i < n) {
-                    // All multiples of i that are even are already handled
-                    // So start from i*i and jump by 2*i
                     for (long long j = (long long)i * i; j < n; j += 2 * i) {
                         isComposite[j] = true;
                     }
