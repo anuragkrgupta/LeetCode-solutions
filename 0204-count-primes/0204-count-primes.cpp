cd8468237754d118ccee40f66f7a6510cd04e72c
Anurag kumar <114020159+anuragkrgupta@users.noreply.github.com>
2026-09-04 22:06:31 +0530

    Time: 568 ms (16.67%), Space: 248.8 MB (5.01%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2)
+            return 0;
+
+        vector<char> isComposite(n, false);
+
+        int count = 1; 
+
+        for (int i = 3; i < n; i += 2) {
+
+            if (!isComposite[i]) {
+                count++;
+
+                if ((long long)i * i < n) {
+                    for (long long j = (long long)i * i; j < n; j += 2 * i) {
+                        isComposite[j] = true;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
